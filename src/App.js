4e070f798a13ad4138ec4efa52458aabdbc89dfc
hungtp1996nd <hungtp1996nd@gmail.com>
2023-06-20 15:30:41 +0700

    update model

--- src/App.js
+++ src/App.js
@@ -13,16 +13,16 @@ const Models = [
 function App() {
   return (
     <div class="wrap-model-view">
-      <model-viewer
+        <model-viewer
+        alt="Neil Armstrong's Spacesuit from the Smithsonian Digitization Programs Office and National Air and Space Museum"
         src={Models[0]}
-        alt="A 3D model of an astronaut"
         ar
-        ar-modes="webxr scene-viewer quick-look"
-        environment-image="neutral"
+        environment-image="shared-assets/environments/moon_1k.hdr"
+        poster="shared-assets/models/NeilArmstrong.webp"
+        shadow-intensity="1"
         camera-controls
-        autoplay
-        class="model-view"
-      />
+        touch-action="pan-y"
+      ></model-viewer>
     </div>
   );
 }
