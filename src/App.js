67d3ac590ebab5dedb8b372d86a3269b4512aa33
0367592801 <phantuanminhnh117@gmail.com>
2023-07-10 16:43:13 +0700

    change style

--- src/App.js
+++ src/App.js
@@ -20,18 +20,20 @@ function App() {
         alt="Neil Armstrong's Spacesuit from the Smithsonian Digitization Programs Office and National Air and Space Museum"
         src="https://res.cloudinary.com/ddpevlrno/image/upload/v1688967615/bee_animation_xqtcmc.glb"
         ar
+        ar-modes="webxr scene-viewer quick-look"
         environment-image="https://modelviewer.dev/shared-assets/environments/moon_1k.hdr"
-        // poster="https://modelviewer.dev/shared-assets/models/NeilArmstrong.webp"
         shadow-intensity="1"
         camera-controls
         touch-action="pan-y"
         className="model-view"
+        autoplay
+        style={{ width: '100%', height: '100vh' }}
         onClick={() => showDetail()}
       >
         <div
           class="hotspot wrap-detail"
           slot="hotspot-hand"
-          data-position="5 4 4"
+          data-position="4 4 2"
           style={{ display: isShowDetail ? "block" : "none" }}
           // data-normal="0 0 0"
         >
