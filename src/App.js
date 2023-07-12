c303cff7fdcddec06bd0da54df1bbdfb7b7c22ac
Minh Phan Tuan <minhphant@nashtechglobal.com>
2023-07-12 09:13:12 +0700

    change model

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
     <div class="wrap-model-view">
       <model-viewer
         alt="Neil Armstrong's Spacesuit from the Smithsonian Digitization Programs Office and National Air and Space Museum"
-        src="https://res.cloudinary.com/ddpevlrno/image/upload/v1688967615/bee_animation_xqtcmc.glb"
+        src="https://res.cloudinary.com/ddpevlrno/image/upload/v1689127833/bee_obj_nh2gtu.glb"
         ar
         ar-modes="webxr scene-viewer quick-look"
         environment-image="https://modelviewer.dev/shared-assets/environments/moon_1k.hdr"
@@ -28,7 +28,7 @@ function App() {
         className="model-view"
         autoplay
         style={{ width: '100%', height: '100vh' }}
-        onClick={() => showDetail()}
+        // onClick={() => showDetail()}
       >
         <div
           class="hotspot wrap-detail"
